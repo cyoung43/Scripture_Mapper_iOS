d935ddec83a907bfe3628e59f87985e95d2db9b1
Chris Young <cyoung43@byu.edu>
2021-11-22 15:17:43 -0800

    adding in map view on the page

--- Scripture_Mapper/View/ScripturesMappedView.swift
+++ Scripture_Mapper/View/ScripturesMappedView.swift
@@ -1,16 +1,21 @@
 //
-//  ContentView.swift
+//  Scripture_Mapper.swift
 //  Scripture_Mapper
 //
 //  Created by Chris Young on 11/22/21.
 //
 
 import SwiftUI
+import MapKit
 
 struct ScripturesMappedView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 31.778389, longitude: 35.234736), span: MKCoordinateSpan(latitudeDelta: 3, longitudeDelta: 3))
+    
     var body: some View {
         NavigationView {
             VolumesView()
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
