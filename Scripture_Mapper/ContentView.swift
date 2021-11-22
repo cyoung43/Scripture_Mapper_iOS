e77d3cd3ea5949d88f00419e0873b85a8df0a8fe
Chris Young <cyoung43@byu.edu>
2021-11-22 09:47:37 -0800

    Initial Commit

---
+++ Scripture_Mapper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scripture_Mapper
+//
+//  Created by New User on 11/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scripture_Mapper/Scripture_MapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scripture_MapperApp.swift
+//  Scripture_Mapper
+//
+//  Created by New User on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Scripture_MapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
