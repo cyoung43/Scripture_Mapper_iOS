306da20601f6b3212af18ea8c3c331307a38b43f
Chris Young <cyoung43@byu.edu>
2021-11-24 13:16:29 -0800

    added a separate map view

--- Scripture_Mapper/View/MapView.swift
+++ Scripture_Mapper/View/MapView.swift
@@ -1,25 +1,24 @@
 //
-//  Scripture_Mapper.swift
+//  MapView.swift
 //  Scripture_Mapper
 //
-//  Created by Chris Young on 11/22/21.
+//  Created by Chris Young on 11/24/21.
 //
 
 import SwiftUI
 import MapKit
 
-struct ScripturesMappedView: View {
+struct MapView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 31.778389, longitude: 35.234736), span: MKCoordinateSpan(latitudeDelta: 3, longitudeDelta: 3))
     
     var body: some View {
-        NavigationView {
-            VolumesView()
-        }
+        Map(coordinateRegion: $region)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        ScripturesMappedView()
+        MapView()
     }
 }
