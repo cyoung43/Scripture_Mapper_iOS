3c9c4c4a8d9d4a988b8056c6b4bec0220051347e
Chris Young <cyoung43@byu.edu>
2021-11-25 07:42:55 -0800

    added title for the chapter view

--- Scripture_Mapper/View/ChapterGridView.swift
+++ Scripture_Mapper/View/ChapterGridView.swift
@@ -12,14 +12,16 @@ struct ChapterGridView: View {
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 8), count: 6), spacing: 8) {
-                ForEach(1...(book.numChapters ?? 0), id: \.self) { chapter in
-                    NavigationLink("\(chapter)") {
-                        ChapterContentView(book: book, chapter: chapter)
-                    }
-                    .isDetailLink(false)
-                }
-            }
+//            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 8), count: 6), spacing: 8) {
+//                ForEach(1...(book.numChapters ?? 0), id: \.self) { chapter in
+//                    NavigationLink("\(chapter)") {
+//                        ChapterContentView(book: book, chapter: chapter)
+//                    }
+//                    .isDetailLink(false)
+//                }
+//            }
+            
         }
+        .navigationBarTitle("\(book.fullName)")
     }
 }
