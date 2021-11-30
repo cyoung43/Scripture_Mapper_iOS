265193aa6ac47e990fe4e49840f8f2be738c9bb5
Chris Young <cyoung43@byu.edu>
2021-11-29 21:58:05 -0700

    adding pins to map

--- Scripture_Mapper/View/MapView.swift
+++ Scripture_Mapper/View/MapView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @EnvironmentObject var scriptureMapper: ScriptureMapper
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 31.778389, longitude: 35.234736), span: MKCoordinateSpan(latitudeDelta: 3, longitudeDelta: 3))
     
     var body: some View {
-        Map(coordinateRegion: $region)
-            .edgesIgnoringSafeArea(.all)
+        Map(coordinateRegion: $region, annotationItems: scriptureMapper.geoPlaces) { geoPlace in
+            MapAnnotation(coordinate: geoPlace.coordinate, anchorPoint: CGPoint(x: 0.5, y: 1)) {
+                Image(systemName: "mappin")
+                    .foregroundColor(Color(red: 0.7, green: 0.1, blue: 0.1))
+                    .shadow(radius: 2, x: 1, y: 1)
+            }
+        } 
     }
 }
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
-}
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}
