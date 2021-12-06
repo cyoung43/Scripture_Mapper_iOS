77c1c891ad88be0d4db1c3d28416c746d7e9f694
Chris Young <cyoung43@byu.edu>
2021-12-06 15:47:16 -0700

    added function to check delta values

--- Scripture_Mapper/ScriptureMapper.swift
+++ Scripture_Mapper/ScriptureMapper.swift
@@ -18,12 +18,21 @@ class ScriptureMapper: ObservableObject, GeoPlaceCollector {
     func setGeocodedPlaces(_ places: [GeoPlace]?) {
         // TO DO: only copy over the unique geoPlaces
         // even if the coordinates are the same, we want to combine the pin and have a comma for the other place name
+        var newPlaces: [GeoPlace]
+        
         if let places = places {
             geoPlaces = places
             
             geoPlaces.forEach { place in
                 print(place.placename)
+                
             }
         }
+    }
+    
+    private func checkDelta(p1: GeoPlace, p2: GeoPlace) -> Bool {
+        let delta = 0.0000001
+        
+        return abs(p1.latitude - p2.latitude) < delta && abs(p1.longitude - p2.longitude) < delta
     }
 }
